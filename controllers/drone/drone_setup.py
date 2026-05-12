5afd03743497fb0b1e1591d8ace9ac5f26df9881
Benjamin Roeser <broes2018@gmail.com>
2026-05-12 18:47:01 +1000

    Seperated the setup and main loop portions of the drone controller into seperate files.

--- controllers/drone/drone_setup.py
+++ controllers/drone/drone_setup.py
@@ -1,104 +1,66 @@
-:x
+from controller import Robot, Keyboard
 
+print("Starting up drone controller...")
 
-#from webots_drone.utils import bytes2image
+def clamp(value, low, high):
+    return max(low, min(value, high))
 
+robot = Robot()
 
-class Drone:
-    def __init__(self, robot):
-        # Time helpers
-        self.time_counter = 0
+timestep = int(robot.getBasicTimeStep())
+dt = timestep / 1000.0
 
-        # Variables
-        self.lift_thrust = 68.5  # with this thrust, the drone lifts.
-        self.init_dist_sensors(robot, int(robot.getBasicTimeStep()))
-        self.init_devices(robot, int(robot.getBasicTimeStep()))
-        self._position = np.array([0.0, 0.0, 0.0])
+fl = robot.getDevice('front left propeller')
+fr = robot.getDevice('front right propeller')
+rl = robot.getDevice('rear left propeller')
+rr = robot.getDevice('rear right propeller')
 
-    def init_dist_sensors(self, drone_node, timestep):
-        """Initialize each sensor distance of the Mavic 2 Pro.
+for m in [fl, fr, rl, rr]:
+    m.setPosition(float('inf'))
+    m.setVelocity(1.0)
 
-        :param drone_node Robot: The instantiated Robot Node class.
-        :param integer timestep: The simulation timestep, 8ms mus be setted,
-            unexpected behaviour can occur with a different value.
-        """
-        self.sensors_id = ['front left dist sonar',
-                           'front right dist sonar',
-                           'rear top dist sonar',
-                           'rear bottom dist sonar',
-                           'left side dist sonar',
-                           'right side dist sonar',
-                           'down front dist sonar',
-                           'down back dist sonar',
-                           'top dist infrared']
-        # instantiate distance sensors
-        self.sensors = list()
-        for sid in self.sensors_id:
-            sensor = drone_node.getDevice(sid)
-            sensor.enable(timestep)
-            self.sensors.append(sensor)
+imu = robot.getDevice('inertial unit'); imu.enable(timestep)
+gps = robot.getDevice('gps');
+gps.enable(timestep)
+gyro = robot.getDevice('gyro');
+gyro.enable(timestep)
+kb = robot.getKeyboard();
+kb.enable(timestep)
 
-        return True
+#camera_roll_motor = robot.getDevice('camera roll')
+#camera_pitch_motor = robot.getDevice('camera pitch')
 
-    def init_devices(self, drone_node, timestep):
-        # Position coordinates [X, Y, Z]
-        self.gps = drone_node.getDevice("gps")
-        self.gps.enable(timestep)
-        print("Initialised the GPS.")
-        ## Angles respect global coordinates [roll, pitch, yaw]
-        #self.imu = drone_node.getDevice("inertial unit")
-        #self.imu.enable(timestep)
-        ## Acceleration angles [roll, pitch, yaw]
-        #self.gyro = drone_node.getDevice("gyro")
-        #self.gyro.enable(timestep)
-        ## Direction degree with north as reference
-        #self.compass = drone_node.getDevice("compass")
-        #self.compass.enable(timestep)
+K_VERTICAL_THRUST = 68.5
+K_VERTICAL_OFFSET = 0.6
+K_VERTICAL_P = 3.0
+K_ROLL_P = 50.0
+K_PITCH_P = 30.0
+TARGET_ALT = 6.0 # metres — auto-takeoff
+ALT_REACHED = 0.1 # metres — tolerance for "reached target"
 
-        # Motors
-        #self.motors_id = ['front left propeller',
-        #                  'front right propeller',
-        #                  'rear left propeller',
-        #                  'rear right propeller']
-        #self.motors = list()
-        #for mid in self.motors_id:
-        #    motor = drone_node.getDevice(mid)
-        #    motor.setPosition(float('inf'))
-        #    motor.setVelocity(1.)
-        #    self.motors.append(motor)
+LAND_SEQUENCE = [
+(4.0, 2.0),
+(2.0, 2.0),
+(0.5, 2.0),
+(0.0, 1.0), # ground — cut motors
+]
 
-        #return True
+# ── State machine constants
+IDLE = 'IDLE'
+TAKEOFF = 'TAKEOFF'
+FLY = 'FLY'
+LAND = 'LAND'
+DONE = 'DONE'
 
-    def get_odometry(self):
-        """Get the drone's current acceleration, angles and position."""
-        orientation = self.imu.getRollPitchYaw()
-        angular_velocity = self.gyro.getValues()
-        position = self.gps.getValues()
-        speed = self.gps.getSpeedVector()
-        compass = self.compass.getValues()
-        north_rad = np.arctan2(compass[1], compass[0])
+# ── State variables
+state = IDLE
+target_altitude = 0.0
+land_stage = 0
+land_stage_timer = 0.0
 
-        return orientation, angular_velocity, position, speed, north_rad
+# ── Wait 1 second for physics to settle
+while robot.step(timestep) != -1:
+    if robot.getTime() > 1.0:
+        break
+print('t=Takeoff l=Auto-land Arrows=fly Shift+Arrows=altitude/strafe')
 
-    def get_dist_sensors(self):
-        """Get the Distance sensors Nodes' measurements."""
-        sensors = dict()
-        for i, sensor_name in enumerate(self.sensors_id):
-            dist_sensor = self.sensors[i]
-            sensors[sensor_name] = [dist_sensor.getValue(),
-                                    dist_sensor.getMinValue(),
-                                    dist_sensor.getMaxValue()]
-        return sensors
-
-    def get_camera_image_shape(self):
-        """Get the camera image dimension and channels."""
-        return (self.camera.getHeight(), self.camera.getWidth(), 4)  # channels
-
-    def set_motors_velocity(self, fl_motor, fr_motor, rl_motor, rr_motor):
-        """Set the drone's motor velocity."""
-        # Actuate over the motors
-        if not np.isnan(fl_motor):
-            self.motors[0].setVelocity(self.lift_thrust + fl_motor)
-            self.motors[1].setVelocity(-(self.lift_thrust + fr_motor))
-            self.motors[2].setVelocity(-(self.lift_thrust + rl_motor))
-            self.motors[3].setVelocity(self.lift_thrust + rr_motor)
