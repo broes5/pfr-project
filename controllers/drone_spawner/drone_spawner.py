17d1113cad6ffe90ca6974271ed24558806d9a54
JakeyDaBob <88590359+JakeyDaBob@users.noreply.github.com>
2026-05-19 12:25:45 +1000

    Drone Spawning

---
+++ controllers/drone_spawner/drone_spawner.py
@@ -0,0 +1,25 @@
+from controller import Supervisor
+
+N_DRONES = 5
+SPACING = 1.2
+START_X = -3.0
+START_Z = 0.065
+
+supervisor    = Supervisor()
+timestep      = int(supervisor.getBasicTimeStep())
+root_children = supervisor.getRoot().getField("children")
+
+for i in range(N_DRONES):
+    x = (i - (N_DRONES - 1) / 2.0) * SPACING
+    node_str = (
+        f'BrickDrone {{ '
+        f'translation {START_X:.4f} {x:.4f} {START_Z:.4f} '
+        f'name "BrickDrone_{i}" '
+        f'}}'
+    )
+    root_children.importMFNodeFromString(-1, node_str)
+
+print(f"[drone_spawner] Spawned {N_DRONES} BrickDrones in a line (spacing={SPACING}m).")
+
+while supervisor.step(timestep) != -1:
+    pass
